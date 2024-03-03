899cefcb21b395f0ae40f9d24bccc4b4b228bb98
Sharath Kumar K <sharathkumar.k00@gmail.com>
2024-03-03 20:16:42 +0530

    Add keybindings for scroll and zoom
    Add keybindings for scroll and zoom; keyboard and mouse

--- config.h
+++ config.h
@@ -173,22 +173,26 @@ static unsigned int defaultattr = 11;
  */
 static uint forcemousemod = ShiftMask;
 
+/* Internal keyboard shortcuts. */
+#define MODKEY Mod1Mask
+#define TERMMOD (ControlMask|ShiftMask)
+
 /*
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ MODKEY,               Button4, kscrollup,      {.i =  4} },
+	{ MODKEY,               Button5, kscrolldown,    {.i =  4} },
+	{ TERMMOD,              Button4, zoom,           {.f = +1} },
+	{ TERMMOD,              Button5, zoom,           {.f = -1} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
-
-/* Internal keyboard shortcuts. */
-#define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
